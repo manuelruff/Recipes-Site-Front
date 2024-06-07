edfcf00b61d18d366eff29b8c04eb42c88d0e8a9
Manuel Ruffieux <manuel42994299@gmail.com>
2024-06-07 14:56:42 +0300

    added username and password to mocklogin

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 // src/services/auth.js
 
 
-  export function mockLogin(credentials, success = true) {
+  export function mockLogin(userName,password, success = true) {
     // Check if already logged in
     if (!success) {
       throw { status: 409, response: { data: { message: "A user is already logged in", success: false } } };
