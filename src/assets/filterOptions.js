a4af5bcb98b5addf584438e42a518b4ef7ff1cdf
Manuel Ruffieux <manuel42994299@gmail.com>
2024-06-13 19:21:56 +0300

    search working

---
+++ src/assets/filterOptions.js
@@ -0,0 +1,59 @@
+export const cuisines = [
+    { value: "African", text: "African" },
+    { value: "Asian", text: "Asian" },
+    { value: "American", text: "American" },
+    { value: "British", text: "British" },
+    { value: "Cajun", text: "Cajun" },
+    { value: "Caribbean", text: "Caribbean" },
+    { value: "Chinese", text: "Chinese" },
+    { value: "Eastern European", text: "Eastern European" },
+    { value: "European", text: "European" },
+    { value: "French", text: "French" },
+    { value: "German", text: "German" },
+    { value: "Greek", text: "Greek" },
+    { value: "Indian", text: "Indian" },
+    { value: "Irish", text: "Irish" },
+    { value: "Italian", text: "Italian" },
+    { value: "Japanese", text: "Japanese" },
+    { value: "Jewish", text: "Jewish" },
+    { value: "Korean", text: "Korean" },
+    { value: "Latin American", text: "Latin American" },
+    { value: "Mediterranean", text: "Mediterranean" },
+    { value: "Mexican", text: "Mexican" },
+    { value: "Middle Eastern", text: "Middle Eastern" },
+    { value: "Nordic", text: "Nordic" },
+    { value: "Southern", text: "Southern" },
+    { value: "Spanish", text: "Spanish" },
+    { value: "Thai", text: "Thai" },
+    { value: "Vietnamese", text: "Vietnamese" }
+  ];
+  
+  export const diets = [
+    { value: "Gluten Free", text: "Gluten Free" },
+    { value: "Ketogenic", text: "Ketogenic" },
+    { value: "Vegetarian", text: "Vegetarian" },
+    { value: "Lacto-Vegetarian", text: "Lacto-Vegetarian" },
+    { value: "Ovo-Vegetarian", text: "Ovo-Vegetarian" },
+    { value: "Vegan", text: "Vegan" },
+    { value: "Pescetarian", text: "Pescetarian" },
+    { value: "Paleo", text: "Paleo" },
+    { value: "Primal", text: "Primal" },
+    { value: "Low FODMAP", text: "Low FODMAP" },
+    { value: "Whole30", text: "Whole30" }
+  ];
+  
+  export const intolerances = [
+    { value: "Dairy", text: "Dairy" },
+    { value: "Egg", text: "Egg" },
+    { value: "Gluten", text: "Gluten" },
+    { value: "Grain", text: "Grain" },
+    { value: "Peanut", text: "Peanut" },
+    { value: "Seafood", text: "Seafood" },
+    { value: "Sesame", text: "Sesame" },
+    { value: "Shellfish", text: "Shellfish" },
+    { value: "Soy", text: "Soy" },
+    { value: "Sulfite", text: "Sulfite" },
+    { value: "Tree Nut", text: "Tree Nut" },
+    { value: "Wheat", text: "Wheat" }
+  ];
+  
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -2,60 +2,74 @@
   <div class="container">
     <h1 class="title">Search Page</h1>
     <nav class="navbar navbar-light bg-light">
-      <form class="form-inline" @submit.prevent="onSearch">
-        <input
-          v-model="query"
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <b-nav-item-dropdown text="Filtering" right class="mr-sm-2">
-          <template v-slot:default>
-            <div class="form-check" v-for="option in dietOptions" :key="option.value">
-              <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.diet" :id="'diet-' + option.value">
-              <label class="form-check-label" :for="'diet-' + option.value">{{ option.text }}</label>
-            </div>
-            <div class="dropdown-divider"></div>
-            <div class="form-check" v-for="option in cuisineOptions" :key="option.value">
-              <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.cuisine" :id="'cuisine-' + option.value">
-              <label class="form-check-label" :for="'cuisine-' + option.value">{{ option.text }}</label>
-            </div>
-            <div class="dropdown-divider"></div>
-            <div class="form-check" v-for="option in intolerancesOptions" :key="option.value">
-              <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.intolerances" :id="'intolerance-' + option.value">
-              <label class="form-check-label" :for="'intolerance-' + option.value">{{ option.text }}</label>
-            </div>
-          </template>
-        </b-nav-item-dropdown>
-        <b-form-select
-          v-model="resultsPerPage"
-          :options="resultsOptions"
-          class="form-control mr-sm-2"
-        ></b-form-select>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-        <b-form-select
-          v-model="sortBy"
-          :options="sortOptions"
-          class="form-control mr-sm-2"
-          @change="onSortChange"
-      ></b-form-select>
+      <form class="form-inline w-100" @submit.prevent="onSearch">
+        <div class="d-flex w-100">
+          <input
+            v-model="query"
+            class="form-control flex-grow-1 mr-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <b-form-select
+            v-model="resultsPerPage"
+            :options="resultsOptions"
+            class="form-control mr-2"
+            style="width: auto;"
+          ></b-form-select>
+          <button class="btn btn-outline-success mr-2" type="submit">
+            Search
+          </button>
+          <b-form-select
+            v-model="sortBy"
+            :options="sortOptions"
+            class="form-control"
+            style="width: auto;"
+            @change="onSortChange"
+          ></b-form-select>
+        </div>
       </form>
     </nav>
-    <div v-if="results.length" class="mt-4">
-      
-      <RecipePreviewList
-        ref="recipeList"
-        title="Search Results"
-        :displayCount="resultsPerPage"
-        class="RandomRecipes center"
-        :recipes="results"
-      />
-    </div>
-    <div v-else class="mt-4">
-      <p>No results found</p>
+    <div class="main-content">
+      <div class="row results">
+        <div v-if="results.length" class="col-12 mt-4">
+          <div class="row">
+            <div class="col-md-6 mb-4 d-flex align-items-stretch" v-for="recipe in results" :key="recipe.id">
+              <RecipePreviewList :recipes="[recipe]" />
+            </div>
+          </div>
+        </div>
+        <div v-else class="col-12 mt-4">
+          <p>No results found</p>
+        </div>
+      </div>
+      <div class="filters">
+        <b-button v-b-toggle.diet-collapse variant="link">Diet</b-button>
+        <b-collapse id="diet-collapse">
+          <div class="form-check" v-for="option in dietOptions" :key="option.value">
+            <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.diet" :id="'diet-' + option.value">
+            <label class="form-check-label" :for="'diet-' + option.value">{{ option.text }}</label>
+          </div>
+        </b-collapse>
+        <div class="dropdown-divider"></div>
+        
+        <b-button v-b-toggle.cuisine-collapse variant="link">Cuisine</b-button>
+        <b-collapse id="cuisine-collapse">
+          <div class="form-check" v-for="option in cuisineOptions" :key="option.value">
+            <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.cuisine" :id="'cuisine-' + option.value">
+            <label class="form-check-label" :for="'cuisine-' + option.value">{{ option.text }}</label>
+          </div>
+        </b-collapse>
+        <div class="dropdown-divider"></div>
+        
+        <b-button v-b-toggle.intolerances-collapse variant="link">Intolerances</b-button>
+        <b-collapse id="intolerances-collapse">
+          <div class="form-check" v-for="option in intolerancesOptions" :key="option.value">
+            <input class="form-check-input" type="checkbox" :value="option.value" v-model="selectedFilters.intolerances" :id="'intolerance-' + option.value">
+            <label class="form-check-label" :for="'intolerance-' + option.value">{{ option.text }}</label>
+          </div>
+        </b-collapse>
+      </div>
     </div>
   </div>
 </template>
@@ -63,6 +77,7 @@
 <script>
 import RecipePreviewList from "../components/RecipePreviewList";
 import { mockGetRecipesPreview2, mockGetSearchResults } from "../services/recipes.js";
+import { cuisines, diets, intolerances } from '../assets/filterOptions';
 
 export default {
   name: "SearchPage",
@@ -81,19 +96,9 @@ export default {
         intolerances: []
       },
       resultsOptions: [5, 10, 15],
-      dietOptions: [
-        { value: "vegetarian", text: "Vegetarian" },
-        { value: "vegan", text: "Vegan" },
-      ],
-      cuisineOptions: [
-        { value: "italian", text: "Italian" },
-        { value: "mexican", text: "Mexican" },
-      ],
-      intolerancesOptions: [
-        { value: "dairy", text: "Dairy" },
-        { value: "gluten", text: "Gluten" },
-        { value: "peanut", text: "Peanut" },
-      ],
+      dietOptions: diets,
+      cuisineOptions: cuisines,
+      intolerancesOptions: intolerances,
       sortOptions: [
         { value: "likes", text: "Likes" },
         { value: "time", text: "Time" },
@@ -149,9 +154,8 @@ export default {
         console.log(error);
       }
 
-      // manu key
+      // API keys
       const apiKey = 'dfc0343255df402babb592636a733295';
-      // omri key
       const dietString = this.selectedFilters.diet.join(',');
       const cuisineString = this.selectedFilters.cuisine.join(',');
       const intolerancesString = this.selectedFilters.intolerances.join(',');
@@ -161,46 +165,42 @@ export default {
       console.log("Cuisine:", cuisineString);
       console.log("Intolerances:", intolerancesString);
       console.log("searching...");
-      // its not doing anything, just for mock
+      // mockGetSearchResults is a placeholder for the actual API call
       mockGetSearchResults(this.query, amountToFetch, dietString, cuisineString, intolerancesString);
     },
-    // this is the api, its working! 80% done, ignore for now
+    // this is the actual API call
     async onSearch2() {
-    // manu key
-    const apiKey = 'dfc0343255df402babb592636a733295';
-    // omri key
-    const dietString = this.selectedFilters.diet.join(',');
-    const cuisineString = this.selectedFilters.cuisine.join(',');
-    const intolerancesString = this.selectedFilters.intolerances.join(',');
-    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${this.query}&number=${this.resultsPerPage}&diet=${dietString}&cuisine=${cuisineString}&intolerances=${intolerancesString}&apiKey=${apiKey}&information=true`;
-    // Log the URL and parameters to the console
-    console.log("Generated URL:", url);
-    console.log("Query:", this.query);
-    console.log("Results Per Page:", this.resultsPerPage);
-    console.log("Diet:", dietString);
-    console.log("Cuisine:", cuisineString);
-    console.log("Intolerances:", intolerancesString);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      this.results = data.results;
-      // Loop through each recipe in the results
-      this.results.forEach(recipe => {
-      // Loop through each variable of the recipe
-      for (const key in recipe) {
-          if (recipe.hasOwnProperty(key)) {
+      const apiKey = 'dfc0343255df402babb592636a733295';
+      const dietString = this.selectedFilters.diet.join(',');
+      const cuisineString = this.selectedFilters.cuisine.join(',');
+      const intolerancesString = this.selectedFilters.intolerances.join(',');
+      const url = `https://api.spoonacular.com/recipes/complexSearch?query=${this.query}&number=${this.resultsPerPage}&diet=${dietString}&cuisine=${cuisineString}&intolerances=${intolerancesString}&apiKey=${apiKey}&information=true`;
+      console.log("Generated URL:", url);
+      console.log("Query:", this.query);
+      console.log("Results Per Page:", this.resultsPerPage);
+      console.log("Diet:", dietString);
+      console.log("Cuisine:", cuisineString);
+      console.log("Intolerances:", intolerancesString);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        this.results = data.results;
+        // Log each recipe's properties
+        this.results.forEach(recipe => {
+          for (const key in recipe) {
+            if (recipe.hasOwnProperty(key)) {
               console.log(`${key}:`, recipe[key]);
+            }
           }
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  },
+    },
     onSortChange() {
       console.log("Sort option changed:", this.sortBy);
       this.sortResults();
-      this.saveState(); // Save state after sorting
+      this.saveState();
     },
     sortResults() {
       if (this.sortBy === "likes") {
@@ -209,14 +209,21 @@ export default {
         this.results.sort((a, b) => a.readyInMinutes - b.readyInMinutes);
       }
     }
+  },
+  watch: {
+    selectedFilters: {
+      handler() {
+        this.saveState();
+      },
+      deep: true
+    }
   }
 };
-
 </script>
 
 <style scoped>
 .container {
-  max-width: 800px;
+  max-width: 1200px;
   margin: 0 auto;
 }
 .title {
@@ -226,12 +233,28 @@ export default {
 .navbar {
   margin-top: 20px;
 }
+.main-content {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+.results {
+  flex: 1;
+  margin-right: 20px;
+}
+.filters {
+  flex-basis: 300px;
+}
+.input-group > .form-control {
+  flex: 1;
+}
+.input-group > .btn, .input-group > .b-form-select {
+  flex: 0 0 auto;
+}
+.d-flex .form-control {
+  flex-grow: 1;
+}
+.d-flex .form-control:not(:last-child) {
+  margin-right: 10px;
+}
 </style>
-
-
-
-
-
-
-
-
