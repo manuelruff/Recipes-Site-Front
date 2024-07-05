e881ef71af1be9f50ed9c8088f2a244a37064683
hallelhel <hallelh@post.bgu.ac.il>
2024-07-05 17:03:47 +0300

    2.1 afterserver working on VPN

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ const state = {
 
     server_domain: "http://localhost:3000",
 
-    // server_domain: "http://132.72.65.211/",
+    //server_domain: "http://manu-omri-recipes.cs.bgu.ac.il",
     
 
 };
